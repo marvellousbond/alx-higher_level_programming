2294a7d35bdd53bc2184859dc63eb42cd509f551
Marvellous Ogege <marvellousogege@gmail.com>
2022-06-05 16:58:34 -0700

    Fix Lists.h

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * is_palindrome - Check if a linked list is a palindrome
+ * @head: The list
+ *
+ * Return: 1 if it's a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
+	listint_t *a = *head;
+	listint_t *b = *head;
 
-	if (head == NULL|| *head == NULL)
+	if (*head == NULL)
 		return (1);
 
-	temp = *head;
-	while (temp) /* get len of list */
+	while (b && b->next && b->next->next)
 	{
-		temp = temp->next;
-		len++;
+		a = a->next;
+		b = b->next->next;
 	}
-	return (0);
+
+	a = reverse_list(&a);
+	b = *head;
+	while (a && b)
+	{
+		if (a->n != b->n)
+			return (0);
+		a = a->next;
+		b = b->next;
+	}
+
+	return (1);
+}
+
+/**
+ * reverse_list - Reverse a linked list
+ * @head: The list
+ *
+ * Return: Pointer to the new head
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
 }
