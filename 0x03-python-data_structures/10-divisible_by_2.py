3cfcf738d88c148ca8af16c1f2d188381c872744
Marvellous Ogege <marvellousogege@gmail.com>
2022-06-05 17:01:41 -0700

    Fix append error

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return my_list
-    new_list = list(my_list)
-    for index in new_list:
-        if index % 2 == 0:
-            new_list[index] = True
-        else:
-            new_list[index] = False
-    return new_list
+    if my_list:
+        list_result = []
+        for i in range(len(my_list)):
+            if my_list[i] % 2:
+                list_result.append(False)
+            else:
+                list_result.append(True)
+        return list_result
