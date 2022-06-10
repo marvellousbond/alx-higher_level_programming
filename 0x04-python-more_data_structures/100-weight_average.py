204dbe47ff5d4e398e69a29c4b00b6636647d543
Marvellous Ogege <marvellousogege@gmail.com>
2022-06-10 16:29:43 -0700

    Fix error

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    res = 0.0
-    s_list = list(t[0] * t[1] for t in my_list)
-    w_list = list(t[1] for t in my_list)
-    res = sum(s_list) / sum(w_list)
-    return res
+    if len(my_list) == 0:
+        return (0)
+    avg = 0
+    weight = 0
+    for x in my_list:
+        avg += x[0] * x[1]
+        weight += x[1]
+    return (avg / weight)
