ef934cb79d5605ac4805306a68d3a7236f601905
Marvellous Ogege <marvellousogege@gmail.com>
2022-06-10 16:36:45 -0700

    Fix error

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
+def mutiply_list_map(my_list=None, number=0):
+    if my_list is None:
+        my_list = []
     return (list(map(lambda i: i * number, my_list)))
